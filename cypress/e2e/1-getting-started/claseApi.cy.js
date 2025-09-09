a0d269ea8f2e4827104dcc4e986f5db736c8d285
Facundo Herrera <facundo.herrera@mi.unc.edu.ar>
2025-09-08 23:42:21 -0300

    Clase Jueves 04092025

---
+++ cypress/e2e/1-getting-started/claseApi.cy.js
@@ -0,0 +1,20 @@
+describe('Clase de APIs en Cypress', () => {
+    it('Clase API', () => {
+        const numrand = Math.floor(Math.random()*1000)
+        cy.intercept('POST','/api/users').as('UsuarioCreado')
+        cy.visit('https://conduit.bondaracademy.com')
+        cy.contains(/sign up/i).click()
+        cy.get('[placeholder="Username"]').type(`FulanitoBen${numrand}`)
+        cy.get('[placeholder="Email"]').type(`francovinci${numrand}@yahoo.com.ar`)
+        cy.get('[placeholder="Password"]').type('1234qwerty')
+        cy.get('.btn').click()
+
+        cy.wait('@UsuarioCreado').then((interception) => {
+            expect(
+                interception.response.statusCode).to.equal(201)
+            }
+            )
+            //cy.get('.error-messages > :nth-child(1)').should('be.visible') //esto fue para cuando repetimos usuario
+
+        })
+    })
