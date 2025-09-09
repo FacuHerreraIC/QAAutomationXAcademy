be4cc45e680554364ea05a513e06e137f23ab873
Facundo Herrera <facundo.herrera@mi.unc.edu.ar>
2025-09-09 00:13:54 -0300

    ActualizacionClase 04092025

--- cypress/e2e/1-getting-started/claseApi.cy.js
+++ cypress/e2e/1-getting-started/claseApi.cy.js
@@ -1,5 +1,5 @@
 describe('Clase de APIs en Cypress', () => {
-    it('Clase API', () => {
+    it.skip('Clase API', () => {
         const numrand = Math.floor(Math.random()*1000)
         cy.intercept('POST','/api/users').as('UsuarioCreado')
         cy.visit('https://conduit.bondaracademy.com')
@@ -16,7 +16,31 @@ describe('Clase de APIs en Cypress', () => {
             //cy.get('.error-messages > :nth-child(1)').should('be.visible') //esto fue para cuando repetimos usuario
             expect(
                 interception.response.body.user).to.have.property('email',`francovinci${numrand}@yahoo.com.ar`)
+                //con esto chequeo si el mail que estoy enviando, es el mismo que la API esta cazando
             }
             )
         })
+        it('MockeoDeBack', () => {
+        cy.intercept('POST','/api/articles*',
+            {
+            body:{
+                "articles": [
+                    {
+                    "title": "Pablito clavo un clavito",
+                    "description": "Una descripcion",
+                    "favoritesCount": 1243
+                    }],
+            "articlesCount": 10
+
+        }
+        }).as('ArticulosDeHoy')
+        cy.visit('https://conduit.bondaracademy.com')
+        })
     })
+// En caso de querer testear valores, datos o informacion del backend y como interacciona con el frontend, 
+// ej: Caso del boton con me gusta, existen 3 formas:
+// a) Me consigo 1000 amigos y pido que carguen me gusta (Incorrecta)
+// b) Le solicito a los pibes de BackEnd que cambien el dato (Incorrectisima)
+// c) Cambiar directamente desde inspeccionar elemento (No aplica correctamente)
+// d) Interceptar el dato desde que volvio del BackEnd, para poder analizar sin cambiar el valor del 
+    //backend (Mockear las validaciones)
